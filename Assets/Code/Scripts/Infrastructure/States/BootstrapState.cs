19daab7c59b734ffc3d34b403a16cae5a69ece7b
ArturApp18 <artur.app19@gmail.com>
2024-07-21 15:57:31 +0200

    Entry Point

---
+++ Assets/Code/Scripts/Infrastructure/States/BootstrapState.cs
@@ -0,0 +1,36 @@
+﻿using Services;
+
+namespace Infrastructure.States
+{
+  public class BootstrapState : IState
+  {
+    private const string Initial = "Initial";
+    private readonly GameStateMachine _stateMachine;
+    private readonly SceneLoader _sceneLoader;
+    private readonly AllServices _services;
+
+    public BootstrapState(GameStateMachine stateMachine, SceneLoader sceneLoader, AllServices services)
+    {
+      _stateMachine = stateMachine;
+      _sceneLoader = sceneLoader;
+      _services = services;
+
+      RegisterServices();
+    }
+
+    public void Enter() =>
+      _sceneLoader.Load(Initial, onLoaded: EnterLoadLevel);
+
+    public void Exit()
+    {
+    }
+
+    private void RegisterServices()
+    {
+      _services.RegisterSingle<IGameStateMachine>(_stateMachine);
+    }
+
+    private void EnterLoadLevel() =>
+      _stateMachine.Enter<LoadLevelState, string>("Main");
+  }
+}
---
+++ Assets/Code/Scripts/Infrastructure/States/LoadLevelState.cs
@@ -0,0 +1,33 @@
+﻿using Logic;
+
+namespace Infrastructure.States
+{
+	public class LoadLevelState : IPayloadedState<string>
+	{
+		private readonly GameStateMachine _stateMachine;
+		private readonly SceneLoader _sceneLoader;
+		private readonly LoadingCurtain _loadingCurtain;
+
+
+		public LoadLevelState(GameStateMachine gameStateMachine, SceneLoader sceneLoader, LoadingCurtain loadingCurtain)
+		{
+			_stateMachine = gameStateMachine;
+			_sceneLoader = sceneLoader;
+			_loadingCurtain = loadingCurtain;
+		}
+
+		public void Enter(string sceneName)
+		{
+			_loadingCurtain.Show();
+			_sceneLoader.Load(sceneName, OnLoaded);
+		}
+
+		public void Exit() =>
+			_loadingCurtain.Hide();
+
+		private void OnLoaded()
+		{
+			_stateMachine.Enter<GameLoopState>();
+		}
+	}
+}
