1a3ed5f119b016869027a48d305dd0627a91383f
ValeriiCSharp <114076774+ValeriiCSharp@users.noreply.github.com>
2024-08-01 00:02:48 +0200

    Edit Beautiful Commit (Refactor)

--- Assets/Code/Scripts/Infrastructure/Mobs/EnemySpawner.cs
+++ Assets/Code/Scripts/Infrastructure/Mobs/EnemySpawner.cs
@@ -12,20 +12,10 @@ namespace Infrastructure.Mobs
 
         private IGameFactory _gameFactory;
         private int _waveNumber;
-        private int _spawnIteration;
 
-        private float _startDelay;
-        private float _delay;
-        private float _mobsSpawnInterval;
-        private EnemyTypeID _enemiesType;
-        private int _countOfEnemies;
-
-
-        private void Awake()
-        {
+        private void Awake() =>
             _gameFactory = AllServices.Container.Single<IGameFactory>();
-        }
-        
+
         public void Spawn() =>
             StartCoroutine(SpawnFromWaveStaticData());
 
@@ -33,7 +23,6 @@ namespace Infrastructure.Mobs
         {
             while (_waveNumber < LevelConfig.WaveConfigs.Count)
             {
-                LoadLevelData();
                 yield return StartCoroutine(MobsSpawnIteration());
                 _waveNumber++;
             }
@@ -42,17 +31,12 @@ namespace Infrastructure.Mobs
         private IEnumerator MobsSpawnIteration()
         {
             if (_waveNumber >= LevelConfig.WaveConfigs.Count)
-            {
-                Debug.LogError("Wave number out of range in MobsSpawnIteration.");
                 yield break;
-            }
 
-            var waveConfig = LevelConfig.WaveConfigs[_waveNumber];
+            WaveConfig waveConfig = LevelConfig.WaveConfigs[_waveNumber];
 
-            for (int i = 0; i < waveConfig.WaveDataList.Count; i++)
+            foreach (WaveData waveData in waveConfig.WaveDataList)
             {
-                WaveData waveData = waveConfig.WaveDataList[i];
-
                 for (int j = 0; j < waveData.EnemiesCount; j++)
                 {
                     _gameFactory.CreateEnemy(waveData.EnemiesType, transform);
@@ -62,14 +46,5 @@ namespace Infrastructure.Mobs
                 yield return new WaitForSeconds(waveData.Delay);
             }
         }
-
-        private void LoadLevelData()
-        {
-            _delay = LevelConfig.WaveConfigs[_waveNumber].WaveDataList[_spawnIteration].Delay;
-            _mobsSpawnInterval = LevelConfig.WaveConfigs[_waveNumber].WaveDataList[_spawnIteration].MobsSpawnInterval;
-            _enemiesType = LevelConfig.WaveConfigs[_waveNumber].WaveDataList[_spawnIteration].EnemiesType;
-            _countOfEnemies = LevelConfig.WaveConfigs[_waveNumber].WaveDataList[_spawnIteration].EnemiesCount;
-        }
-        
     }
 }
