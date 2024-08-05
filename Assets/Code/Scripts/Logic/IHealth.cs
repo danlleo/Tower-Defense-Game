566a417267dd9ace8766ca900db04555604826ec
ArturApp18 <artur.app19@gmail.com>
2024-08-05 21:55:29 +0200

    IHealth

---
+++ Assets/Code/Scripts/Logic/IHealth.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Logic
+{
+
+	public interface IHealth
+	{
+		event Action HealthChanged;
+		float Current { get; set; }
+		float Max { get; set; }
+		void TakeDamage(float damage);
+	}
+}
